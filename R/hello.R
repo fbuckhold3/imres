4542b54980d5e60a421cbd04434556955c8bbc86
fbuckhold3 <78330963+fbuckhold3@users.noreply.github.com>
2025-01-18 09:41:18 -0600

    Updated files

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -12,6 +12,13 @@
 #   Install Package:           'Cmd + Shift + B'
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
+#
+install.packages(c("usethis", "devtools", "roxygen2", "testthat", "pkgdown"))
+library(usethis)
+library(devtools)
+library(roxygen2)
+library(testthat)
+library(pkgdown)
 
 hello <- function() {
   print("Hello, world!")
