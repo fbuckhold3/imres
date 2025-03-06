86a3a00eae3c9e8777efa0827bff707d1d96cef4
fbuckhold3 <78330963+fbuckhold3@users.noreply.github.com>
2025-03-05 19:49:18 -0600

    Updated modules...

---
+++ R/modules_ccc_milestones.R
@@ -0,0 +1,15 @@
+#' @importFrom shiny NS selectInput moduleServer
+
+#' @export
+# Module for period selection
+mod_miles_select_ui <- function(id) {
+  ns <- NS(id)
+  selectInput(ns("period"), "Select Period", choices = c("Entering Residency", "Mid Intern", "End Intern", "Mid PGY2", "End PGY2", "Mid PGY3", "Graduating", "Interim Review"))
+}
+
+#' @export
+mod_miles_select_server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    reactive(input$period)
+  })
+}
